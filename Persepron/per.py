4f30df5d875875a52705d1f51a25a57dc8f0172f
zahra-zarrabi <zahra.zarrabi1998@gmail.com>
2021-08-23 23:04:42 +0430

    3d

--- Persepron/per.py
+++ Persepron/per.py
@@ -2,33 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-# def data_generator(n):
-#     X=np.random.uniform(0,40,n)
-#     Y=X+np.random.normal(0,2,n)
-#
-#     X=X.reshape(-1,1)
-#     Y=Y.reshape(-1,1)
-#
-#     return X,Y
-# n=200
-# X_train,Y_train =data_generator(n)
-# lr=0.0001
-# m=np.random.rand(1,1)
-# fig,ax=plt.subplots()
-# for i in range(n):
-#     #train
-#     y_pred=np.matmul(X_train[i],m)
-#     e=np.subtract(Y_train[i],y_pred)
-#     m=m+e*lr*X_train[i]
-#     #plot
-#     y_pred=np.matmul(X_train,m)
-#     ax.clear()
-#     plt.scatter(X_train,Y_train,c='red')
-#     ax.plot(X_train,y_pred,c='blue',lw=2)
-#     plt.pause(0.1)
-#
-# print('X_train',X_train.shape)
-# print('y_train',Y_train.shape)
 
 train_data=np.array(pd.read_csv('linear_data_train.csv'))
 test_data=np.array(pd.read_csv('linear_data_test.csv'))
@@ -37,12 +10,7 @@ X_train =train_data[:, 0:2]
 Y_train = train_data[:, 2]
 X_test =test_data[:,0:2]
 Y_test= test_data[:,2]
-# Y_train= Y_train.reshape(-1,1)
-# Y_test= Y_test.reshape(-1,1)
-# class Perseptron:
-#     def __init__(self):
-#         pass
-#     def train(self):
+
 
 N=X_train.shape[0]
 lr=0.01
